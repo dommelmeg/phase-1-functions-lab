eddc7e6fbbbd3f46fc6850cbc40b47b100555a9c
Megan <dommelmeg@gmail.com>
2022-10-24 13:43:06 -0400

    commit tree 1fb7d312cc4a1718212432cbb7882ef99a2be230

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const blocks = Math.abs(42 - pickupLocation);
+
+    return blocks;
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feet = blocks * 264
+
+    return feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination);
+    const feet = blocks * 264
+
+    return feet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const blocks = Math.abs(start - destination);
+    const feet = blocks * 264
+
+    let fare;
+    if (feet <= 399) {
+        fare = 0;
+    }    else if (feet >= 400, feet <= 2000) {
+            fare = (feet - 400) * 0.02;
+    }    else if (feet >= 2001, feet <= 2500) {
+                fare = 25;
+    }    else {
+                fare = 'cannot travel that far';
+    }
+
+    return fare;
+}
